0fc4848db62e8706a986aa723dbf6fe2f445db7b
Olivier Locard <olivier.locard@deveryware.com>
2020-02-13 19:17:08 +0100

    MatomoRequester.params creation.

--- matomo/matomo_requester.py
+++ matomo/matomo_requester.py
@@ -10,6 +10,14 @@ class MatomoRequester(object):
         self.args = args
         self.kwargs = kwargs
 
+    @property
+    def params(self) -> str:
+        k = '&'.join(self._get_kwargs())
+        a = '&'.join(self.args)
+        if k and a:
+            return '{}&{}'.format(k, a)
+        return k or a
+
     def _get_kwargs(self) -> list:
         l = list()
         for arg in self.kwargs:
--- tests/matomo/test_matomo_requester.py
+++ tests/matomo/test_matomo_requester.py
@@ -13,3 +13,10 @@ def test_matomo_requester_instance(matomo_requester):
 def test_get_kwargs(matomo_requester):
     assert isinstance(matomo_requester._get_kwargs(), list) is True
     assert MatomoRequester(1, 'a', field='test', account='oloc')._get_kwargs() == ['field=test', 'account=oloc']
+
+
+def test_params(matomo_requester):
+    assert isinstance(matomo_requester.params, str) is True
+    assert (MatomoRequester('a', 'b', field='test', account='oloc')).params == 'field=test&account=oloc&a&b'
+    assert (MatomoRequester(field='test', account='oloc')).params == 'field=test&account=oloc'
+    assert (MatomoRequester('a', 'b')).params == 'a&b'
