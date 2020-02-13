53f35484839a5b4218cc1aadd7e5ac9599a8be2d
Olivier Locard <olivier.locard@deveryware.com>
2020-02-13 17:45:27 +0100

    Add MatomoMethod __str__.

--- matomo/matomo_method.py
+++ matomo/matomo_method.py
@@ -9,3 +9,7 @@ class MatomoMethod(object):
     def __init__(self, name, module):
         self.name = name
         self.module = module
+
+    def __str__(self):
+        return "{connector}&method={module}.{method}".format(connector=self.module.connector, module=self.module.name,
+                                                             method=self.name)
--- tests/matomo/test_matomo_method.py
+++ tests/matomo/test_matomo_method.py
@@ -8,3 +8,7 @@ def test_matomo_method_instance(matomo_method):
     assert isinstance(matomo_method, object) is True
     assert hasattr(matomo_method, 'name') is True
     assert hasattr(matomo_method, 'module') is True
+
+
+def test_str(matomo_method):
+    assert str(matomo_method) == 'url/?token_auth=token&module=API&idSite=all&method=module.method'
