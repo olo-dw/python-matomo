0c26ad210715c72fe69a6efbfc13f41ecddb8f8d
Olivier Locard <olivier.locard@deveryware.com>
2020-02-13 17:31:40 +0100

    Method attribut of Module.

--- matomo/matomo_module.py
+++ matomo/matomo_module.py
@@ -3,9 +3,14 @@
 #
 # Olivier Locard
 
+from .matomo_method import MatomoMethod
+
 
 class MatomoModule(object):
 
     def __init__(self, name: str, connector):
         self.name = name
         self.connector = connector
+
+    def __getattr__(self, item: str) -> MatomoMethod:
+        return MatomoMethod(item, self)
--- tests/matomo/test_matomo_module.py
+++ tests/matomo/test_matomo_module.py
@@ -3,8 +3,14 @@
 #
 # Olivier Locard
 
+from matomo.matomo_method import MatomoMethod
+
 
 def test_matomo_module_instance(matomo_module):
     assert isinstance(matomo_module, object) is True
     assert hasattr(matomo_module, 'connector') is True
     assert hasattr(matomo_module, 'name') is True
+
+
+def test_getattr(matomo_module):
+    assert isinstance(matomo_module.method, MatomoMethod) is True
