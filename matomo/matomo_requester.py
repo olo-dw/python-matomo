069d850f0ff2b303e01fda945d105e010a37930b
Olivier Locard <olivier.locard@deveryware.com>
2020-02-13 19:05:26 +0100

    MatomoRequester._get_kwargs() creation

--- matomo/matomo_requester.py
+++ matomo/matomo_requester.py
@@ -9,3 +9,9 @@ class MatomoRequester(object):
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
+
+    def _get_kwargs(self) -> list:
+        l = list()
+        for arg in self.kwargs:
+            l.append("{}={}".format(arg, self.kwargs[arg]))
+        return l
--- tests/matomo/test_matomo_requester.py
+++ tests/matomo/test_matomo_requester.py
@@ -3,6 +3,13 @@
 #
 # Olivier Locard
 
+from matomo.matomo_requester import MatomoRequester
+
 
 def test_matomo_requester_instance(matomo_requester):
     assert isinstance(matomo_requester, object) is True
+
+
+def test_get_kwargs(matomo_requester):
+    assert isinstance(matomo_requester._get_kwargs(), list) is True
+    assert MatomoRequester(1, 'a', field='test', account='oloc')._get_kwargs() == ['field=test', 'account=oloc']
